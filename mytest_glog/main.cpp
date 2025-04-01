31bc9da0a94cc294bf935fc74dd159ffbad52907
2197947404@qq.com <rzz-rz@rzzrz.>
2025-04-01 08:59:58 +0800

    test installprefixformatter()

--- mytest_glog/main.cpp
+++ mytest_glog/main.cpp
@@ -1,9 +1,19 @@
 #include <glog/logging.h>
 #include <iostream>
+#include <ostream>
+
+// void myformatter(std::basic_ostream<char>& ostream ,const google::LogMessage& messageinfo,void* ptr)
+// {
+//     std::cout<<"this is my formatter"<<std::endl;
+// }
+
 int main(int argc, char* argv[]){
     google::InitGoogleLogging(argv[0]);
     int num_cookies = 1;
-    LOG(INFO) << "Found " << num_cookies << " cookies"; // (2)!
+    
+    //google::InstallPrefixFormatter(myformatter);
+    LOG_IF(INFO, num_cookies > 10) << "Got lots of cookies";
     std::cout<<"glog success"<<std::endl;
+
     return 0;
 }
